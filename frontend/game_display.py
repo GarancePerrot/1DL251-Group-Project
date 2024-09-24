7eebd476ec4fdb9ebe83530ed1f3e46ddd830da6
filmal12 <filip.malamas@gmail.com>
2024-09-24 15:34:52 +0200

    start of ui

--- frontend/game_display.py
+++ frontend/game_display.py
@@ -1,1 +1,20 @@
 # TEST FEEL FREE TO DELETE ONLY FOR FOLDER STRUCTURE
+import pygame
+
+def run_game():
+    pygame.init()
+    screen = pygame.display.set_mode((1200, 800))
+    pygame.display.set_caption('GROUP-PROJECT')
+
+    running = True
+
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+
+
+        screen.fill((0, 0, 0))
+
+        pygame.display.flip() 
+    pygame.quit()
--- main.py
+++ main.py
@@ -1,1 +1,6 @@
 # HERE START RUNNING PROGRAM
+from frontend.game_display import run_game
+
+
+if __name__ == "__main__":
+    run_game()
